2547101ecfd51b986cddbf5bbea1df848d05febd
yw1309 <yw1309@nyu.edu>
2016-11-18 20:56:41 -0500

    changes

---
+++ final-project/db.js
@@ -0,0 +1,46 @@
+// db.js
+
+var mongoose = require('mongoose'), 
+URLSlugs = require('mongoose-url-slugs');
+
+// my schema goes here!
+var Post = new mongoose.Schema({
+  user: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
+  title: {type: String, required: true},
+  location: {type: String, required: true},
+  date: {type: Date, default: Date.now},
+  content: {type: String, required: true},
+  public: {type: Boolean, default: false},
+});
+
+var Homepage = new mongoose.Schema({
+  name: {type: String},
+  posts: [Post]
+});
+
+Post.plugin(URLSlugs('title'));
+
+mongoose.model('Post', Post);
+mongoose.model('Homepage', Homepage);
+
+// is the environment variable, NODE_ENV, set to PRODUCTION? 
+if (process.env.NODE_ENV == 'PRODUCTION') {
+ // if we're in PRODUCTION mode, then read the configration from a file
+ // use blocking file io to do this...
+ var fs = require('fs');
+ var path = require('path');
+ var fn = path.join(__dirname, 'config.json');
+ var data = fs.readFileSync(fn);
+
+ // our configuration file will be in json, so parse it and set the
+ // conenction string appropriately!
+ var conf = JSON.parse(data);
+ var dbconf = conf.dbconf;
+} else {
+ // if we're not in PRODUCTION mode, then use
+ dbconf = 'mongodb://localhost/final';
+}
+
+mongoose.connect(dbconf);
+
+// mongoose.connect('mongodb://localhost/final');
--- final-project/routes/index.js
+++ final-project/routes/index.js
@@ -1,9 +1,64 @@
+var db = require('.././db');
+var mongoose = require('mongoose');
+
+var Post = mongoose.model('Post');
+var Homepage = mongoose.model('Homepage');
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index');
 });
+
+
+router.get('/new-post', function(req, res, next) {
+	res.render('create');
+});
+
+router.post('/add-post', function(req, res, next) {
+	new Post({
+		title: req.body.title,
+		location: req.body.location,
+		content: req.body.content
+	}).save(function(err, post){
+		res.redirect('/view-posts');
+	});
+
+});
+
+
+router.get('/view-posts', function(req, res, next) {	
+	Post.find(function(err, posts, count) {
+		res.render( 'view', {'slug': posts.slug, 'posts': posts});
+	});
+});
+
+router.get('/view-posts/:slug', function(req, res, next) {
+	Post.find({'slug' : req.params.slug}, function(err, post, count) {
+		res.render('viewone', {slug: post.slug,'post': post});
+	});
+});
+
+
+router.get('/edit/:slug', function(req, res, next) {
+	Post.find({'slug' : req.params.slug}, function(err, post, count) {
+		res.render('edit', {slug: post.slug,'post': post});
+	});
+});
+
+router.post('/update/:slug', function(req, res, next) {
+	Post.findOneAndUpdate({'slug' : req.params.slug}, 
+		{$set: {title: req.body.title, location: req.body.location, 
+			content: req.body.content}},
+		function(err, posts, count) {
+
+			var strURL = '/view-posts/'+req.params.slug;
+			res.redirect(strURL);
+		});
+
+});
+
 
 module.exports = router;
